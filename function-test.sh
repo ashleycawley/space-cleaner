6e1bf24985663870ab546dbdb764f4b0a2144085
Ashley Cawley <ash@ashleycawley.co.uk>
2019-04-17 15:06:20 +0100

    2nd test

--- function-test.sh
+++ function-test.sh
@@ -14,7 +14,11 @@ Free Disk space prior to cleaning: $FreeSpaceBeforeClean"
 FreeSpaceAfterClean=`df -k -h . | awk ' { print $4 } ' | sed -e 's/Avail//g' | tail -n 1`
 
 # Strips the M (for MB), G (for GB) and T (for Terabyte) out of the results, just leaving a integer
+BEFORE=`echo $FreeSpaceBeforeClean | SanitiseMeasurement`
+AFTER=`echo $FreeSpaceAfterClean | SanitiseMeasurement`
 
-echo $FreeSpaceAfterClean | SanitiseMeasurement
+FREED=`echo "$BEFORE-$AFTER" | bc`
+
+echo "Space Released: $FREED"
 
 exit 0
