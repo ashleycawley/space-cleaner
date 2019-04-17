333d415fdbf0565f54bf64d8ba606da765a48b75
Ashley Cawley <ash@ashleycawley.co.uk>
2019-04-17 14:55:08 +0100

    Testing

--- function-test.sh
+++ function-test.sh
@@ -3,8 +3,7 @@
 FreeSpaceBeforeClean=`df -k -h . | awk ' { print $4 } ' | sed -e 's/Avail//g' | tail -n 1`
 
 function SanitiseMeasurement {
-
-    
+    sed s/M//g | sed s/G//g | sed s/T//g
 }
 
 echo -e "
@@ -15,6 +14,7 @@ Free Disk space prior to cleaning: $FreeSpaceBeforeClean"
 FreeSpaceAfterClean=`df -k -h . | awk ' { print $4 } ' | sed -e 's/Avail//g' | tail -n 1`
 
 # Strips the M (for MB), G (for GB) and T (for Terabyte) out of the results, just leaving a integer
-echo $FreeSpaceAfterClean | sed s/M//g | sed s/G//g | sed s/T//g
+
+echo $FreeSpaceAfterClean | SanitiseMeasurement
 
 exit 0
